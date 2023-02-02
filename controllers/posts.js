25f822a2758df134d7036ee25b9d25692abf5a0d
Sukhdeep Singh <sukhdsv@gmail.com>
2023-02-01 18:54:25 -0800

    Adds delete post functionality on the All Posts view

--- controllers/posts.js
+++ controllers/posts.js
@@ -76,13 +76,15 @@ module.exports = {
       // Find post by id
       let post = await Post.findById({ _id: req.params.id });
       // Delete image from cloudinary
-      await cloudinary.uploader.destroy(post.cloudinaryId);
+      if (post.cloudinaryId != null) {  
+        await cloudinary.uploader.destroy(post.cloudinaryId);
+      }
       // Delete post from db
       await Post.remove({ _id: req.params.id });
       console.log("Deleted Post");
-      res.redirect("/profile");
+      res.redirect("/post/allposts");
     } catch (err) {
-      res.redirect("/profile");
+      res.redirect("/post/allposts");
     }
   },
 };
