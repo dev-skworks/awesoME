484a3c4f6b28f4ad0650c194bd4270b1b5ef329c
Sukhdeep Singh <sukhdsv@gmail.com>
2023-03-16 15:57:33 -0700

    Implements light theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
         'light-main': '#fffffe',
         'primary': '#ff8906',
         'secondary': '#e53170',
-        'highlight-text': '#a7a9be'
+        'highlight-text': '#a7a9be',
+        'heading-light': '#00214d',
+        'primary-light': '#00ebc7',
+        'secondary-light': '#ff5470',
+        'highlight-text-light': '#1b2d45'
       }
     },
   },
